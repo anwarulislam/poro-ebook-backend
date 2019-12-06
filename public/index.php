77fd62d4328ad2100269ade9535f9a5332d17f91
Tech <toufiq.austcse@gmail.com>
2019-12-06 21:55:18 +0600

    changes

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@
  */
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Authorization');
+header('Access-Control-Allow-Headers: *');
 // header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 
 define('LARAVEL_START', microtime(true));
